58caed474c6e4616a05e79990f1633da4ad87ded
Bishop <christianiradukunda6@gmail.com>
2023-09-09 19:13:31 +0200

    translated the flight scenario and did the tests

---
+++ Domain.Tests/Flight.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Tests
+{
+    public class Flight
+    {
+        public int remainingNumberOfSeats { get; set; }
+        public Flight(int SeatCapacity)
+        {
+            remainingNumberOfSeats = SeatCapacity;
+
+        }
+        public void Book(string V1, int V2)
+        {
+            remainingNumberOfSeats -= V2;
+        }
+
+    }
+}
--- FlightTest/UnitTest1.cs
+++ FlightTest/UnitTest1.cs
@@ -5,6 +5,10 @@ namespace FlightTest
         [Fact]
         public void Test1()
         {
+            var flight = new Flight(SeatCapacity: 3);
+            flight.Book("chris@gmail.com", 1);
+
+            flight.remainingNumberOfSeats.Should().Be(2);
 
         }
     }
